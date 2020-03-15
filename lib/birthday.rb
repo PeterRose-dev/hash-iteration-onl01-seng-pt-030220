bf98dc8d00c5a30a8ec1fe38751ef9a416f03be7
Peter Rose <peterroseq1@gmail.com>
2020-03-15 16:50:07 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -22,7 +22,7 @@ def age_appropriate_birthday(birthday_kids)
     elsif kids_name["Sarah"]
         puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else
-        puts nil
+        puts "You are too old for this."
 end
 end
 end
